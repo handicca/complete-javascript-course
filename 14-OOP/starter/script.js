2cb2c564d5046c94e48007b26741e708f3a478d6
Handika <handika00026@gmail.com>
2024-04-24 21:03:53 +0700

    complete coding challenge p1 OOP

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -21,3 +21,122 @@ menggunakan, dan prilaku ini disebut prototype inheritance.
 */
 
 // 3 ways of implementing prototypal inheritance in javascript: Constructor functions, ES6 classes, Object.create()
+
+// Constructor functions and the new operator
+// Arrow function tidak bisa untuk constructor functions karena tidak memiliki this
+
+const Person = function (firstName, birthYear) {
+  // instance properties
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+
+  // Never do this cuz ini akan membuat salinan dan bad performance
+  // Better menggunakan prototype inheritance
+  // this.calcAge = function () {
+  //     console.log(2037 - this.birthYear);
+  // }
+};
+
+const handika = new Person('Handika', 2004);
+console.log(handika);
+console.log(handika instanceof Person); // true
+
+// BTS new keyword
+// 1. New empty {} created
+// 2. function is called, this = {}
+// 3. {} linked to prototype
+// 4. function automatically return {}
+
+/*
+Di JavaScript, kata kunci new digunakan bersamaan dengan fungsi konstruktor untuk membuat instance baru 
+dari sebuah objek. Ketika Anda menggunakan new sebelum memanggil sebuah fungsi, itu menunjukkan kepada 
+JavaScript bahwa Anda ingin membuat sebuah objek baru yang dibuat berdasarkan fungsi tersebut.
+
+Di sini, Person adalah sebuah fungsi konstruktor yang akan membuat objek dengan properti firtsName dan 
+birthYear saat dipanggil dengan new. new menginstruksikan JavaScript untuk membuat sebuah objek baru dan 
+menghubungkannya dengan fungsi konstruktor yang diberikan, sehingga setiap kali Anda memanggil new Person
+(...), Anda membuat objek baru dengan properti yang sesuai.
+*/
+
+// Prototypes
+/*
+Setiap function dlm Javascript secara otomatis memiliki properti yg disebut prototype.
+*/
+Person.prototype.calcAge = function () {
+  console.log(2037 - this.birthYear);
+};
+
+handika.calcAge();
+
+console.log(handika.__proto__);
+console.log(handika.__proto__ === Person.prototype); // true
+
+console.log(Person.prototype.isPrototypeOf(handika)); // true
+console.log(Person.prototype.isPrototypeOf(Person)); // false
+
+// set property
+// property ini tidak berada di object yang dibuat melainkan didalam prototype tetapi object tersebut dapat mengakses property tersebut.
+Person.prototype.species = 'Homo Sapiens';
+console.log(handika.species);
+console.log(handika.hasOwnProperty('firstName')); // true
+console.log(handika.hasOwnProperty('species')); // false
+
+console.log(handika.__proto__);
+// Object.prototype (top of prototype chain)
+console.log(handika.__proto__.__proto__);
+console.log(handika.__proto__.__proto__.__proto__); // null
+
+console.dir(Person.prototype.constructor); // return diri sendiri
+
+const arr = [3, 6, 5, 6, 4, 9, 9, 4]; // new Array = []
+console.log(arr.__proto__);
+console.log(arr.__proto__ == Array.prototype); // true
+
+// Object.prototype (top of prototype chain)
+console.log(arr.__proto__.__proto__);
+
+// menambah method array pada object prototype
+// tetapi jangan dibiasakan melakukan ini
+Array.prototype.unique = function () {
+  return [...new Set(this)];
+};
+
+console.log(arr.unique());
+
+///////////////////////////////////////
+// Coding Challenge #1
+console.log('===== Coding Challenge #1 =====');
+/* 
+1. Use a constructor function to implement a Car. A car has a make and a speed property. The speed property is the current speed of the car in km/h;
+2. Implement an 'accelerate' method that will increase the car's speed by 10, and log the new speed to the console;
+3. Implement a 'brake' method that will decrease the car's speed by 5, and log the new speed to the console;
+4. Create 2 car objects and experiment with calling 'accelerate' and 'brake' multiple times on each of them.
+
+DATA CAR 1: 'BMW' going at 120 km/h
+DATA CAR 2: 'Mercedes' going at 95 km/h
+
+GOOD LUCK 😀
+*/
+
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+  console.log(`"${this.make}" going at ${this.speed} km/h`);
+};
+
+Car.prototype.brake = function () {
+  this.speed -= 5;
+  console.log(`"${this.make}" going at ${this.speed} km/h`);
+};
+
+const bmw = new Car('BMW', 120);
+const mercedes = new Car('Mercedes', 95);
+
+bmw.accelerate();
+bmw.brake();
+mercedes.accelerate();
+mercedes.brake();
